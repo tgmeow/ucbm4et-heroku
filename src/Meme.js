91ce8b11b6f41e2258314f8686bbace6e87c39f2
Tiger Mou <tigermou16@gmail.com>
2017-09-04 18:19:13 -0500

    modified Meme component structure for better mobile compatibility and more bootstrap usage

--- src/Meme.js
+++ src/Meme.js
@@ -3,17 +3,34 @@
 import React, { Component } from 'react';
 
 const wholeDivS = {
-    margin: '10px', clear: 'both', backgroundColor: '#eee',
-    overflow: 'hidden' //temp fix for div size
+    width:'100%',
+    clear: 'both',
+    backgroundColor: '#eee',
+    display: 'inline-block'
 };
 
-const left = { float: 'left', padding: '20px' };
+const left = {
+    float: 'left',
+    margin: '1em 0px 0px 2em',
+};
 
-const right = { float: 'left', padding: '10px', width: '75%' };
+const textblock = {
+    margin:'1em 2em'
+}
 
-const paraS = { margin: '0', padding: '10px', paddingTop: '0px' };
+const right = {
+    float: 'left',
+    width:'90%'
+};
 
-const imgS = { height: '300px' };
+const imgDiv = {
+    clear: 'both',
+    marginBottom:'1em',
+};
+const imgSty = {
+    marginLeft:'auto',
+    marginRight:'auto'
+};
 
 
 class Meme extends Component {
@@ -26,13 +43,14 @@ class Meme extends Component {
         let image = '';
         if (this.props.full_picture) {
             image = <img
-                style={imgS}
+                className= 'img-responsive'
+                style={imgSty}
                 src={this.props.full_picture}
-                alt={"No meme provided."}
+                alt={'Meme.'}
             />
         }
 
-        let video = this.props.type === 'video' ? 'Video' : '';
+        let video = this.props.type === 'video' ? (<strong>You can view this video in Facebook.</strong>) : '';
 
         let today = new Date();
         let postDT = this.props.created_time.split('.')[0].split('T');
@@ -42,9 +60,14 @@ class Meme extends Component {
         //UTC date construction needs a month on 0 based indexing
         let postActualDate = new Date(Date.UTC(postDate[0], postDate[1] - 1, postDate[2], postTime[0], postTime[1], postTime[2]));
 
+        //create a string for the time 
         var stringDiff = '';
+        const DIFF_STRING_PRE = '';
+        const DIFF_STRING_POST = ' ago on '
         let diffHours = Math.round((today - postActualDate) / 3600000);
-        if (diffHours <= 24) stringDiff = diffHours + ' hours ago'
+        if (diffHours <= 24) stringDiff = diffHours + ' hours'
+        else stringDiff = Math.round(diffHours/24) + ' days'
+        stringDiff = DIFF_STRING_PRE + stringDiff + DIFF_STRING_POST;
 
         let link = 'https://www.facebook.com/' + this.props.id;
 
@@ -55,20 +78,21 @@ class Meme extends Component {
                 </div>
 
                 <div style={right}>
-                    <span>
-                        <a href={link} rel='noopener noreferrer' target='_blank'>Open in Facebook</a>
-
-                    </span>
-                    <p style={paraS}>
-                        {this.props.message}
-                    </p>
-                    <span>{stringDiff}</span>
-                    <br />
-                    <span>{postActualDate.toString()}</span>
-                    <div style={{ clear: 'both' }}>
+                    <div style={textblock}>
+                        <span>
+                            <a href={link} rel='noopener noreferrer' target='_blank'>Open in Facebook</a>
+                        </span>
+                        <p className='lead'>
+                            {this.props.message}
+                        </p>
+                        <span>{stringDiff} {postActualDate.toString().split(' GMT')[0]}</span>
+                        <br/>
                         {video}
-                        {image}
                     </div>
+   
+                </div>
+                <div style={imgDiv}>
+                    {image}
                 </div>
 
             </div>
